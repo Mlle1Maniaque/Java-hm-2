8c9b850c6ab11e849f641dc401b96169ef0356cd
Mlle Maniaque <Mlle1Maniaque@yandex.ru>
2023-04-03 19:13:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15550;
+        int perMile = 20;
+        int bonus = amount / perMile;
+
+        System.out.println("Благодарим за покупку! Вам будет начислено " + bonus + " бонуса (-ов)");
+
+
+    }
+}
